76ae8fb6918cf303f140644d32c25c27fd150f32
unknown <alexandre8603@gmail.com>
2022-06-07 13:03:56 -0300

    form

---
+++ front-end/src/services/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDS7eWfNWxtQQkZnv_utyAV3NIK7Ssj47g",
+
+  authDomain: "nossa-menina.firebaseapp.com",
+
+  projectId: "nossa-menina",
+
+  storageBucket: "nossa-menina.appspot.com",
+
+  messagingSenderId: "132345740506",
+
+  appId: "1:132345740506:web:1b388f3b3cd7c3518a79e3"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig)
--- front-end/src/views/home.tsx
+++ front-end/src/views/home.tsx
@@ -1,13 +1,26 @@
-import { Container } from "react-bootstrap";
+import { Container, Form, FormControlProps } from "react-bootstrap";
 import styled from "styled-components";
 import bckground from "../../src/assets/img/papel.jpg"
 import logo from "../../src/assets/img/Quer nos ajudar.png"
 
-export function Home () {
+type Props = FormControlProps
+
+export function Home ( props : Props ) {
     return (
         <Bckgroud className="vh-100 ">
             <Container className="d-flex flex-column align-items-center">
                 <img src={logo} className='mt-4' width="290px" height="270px"/>
+                <Form>
+                <Form.Control {...props}
+                type="text"
+                label="ola"
+                placeholder="Digite seu nssome"
+              />
+              <Form.Control {...props}
+                label="Sua sugestão"
+                placeholder="Digite o nome da nossa menina."
+                />
+                </Form>
             </Container>
         </Bckgroud>
     )
